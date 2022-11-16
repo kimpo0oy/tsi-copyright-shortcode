0635f22ffebc32355dd409c1960bc2180b7f81b5
Kevin Steve Maningo <118400742+kimpo0oy@users.noreply.github.com>
2022-11-17 06:45:17 +0800

    Update copyright-shortcode.php

--- copyright-shortcode.php
+++ copyright-shortcode.php
@@ -4,13 +4,13 @@
  *
  * @package       COPYRIGHTS
  * @author        Kevin Steve Maningo
- * @version       1.2
+ * @version       1.3
  *
  * @wordpress-plugin
  * Plugin Name:   Copyright Shortcode
  * Plugin URI:    https://www.thesavvyinspector.com
  * Description:   Usage: [copyright] will display the whole Copyright, [copyright-only] will display the Copyright without the created by, [created-by] will display the Created by The Savvy Tech without the copyright.
- * Version:       1.2
+ * Version:       1.3
  * Author:        THE SAVVY INSPECTOR | Kevin Steve Maningo
  * Author URI:    https://www.thesavvyinspector.com
  * Text Domain:   copyright-shortcode
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 // Add Shortcode
 function get_copyright() {
 
-    return '&copy; Copyright ' . date('Y') . ' ' . get_bloginfo('name') . '<br/>All Rights Reserved | Created By: <a href="https://getmemorehomeinspectionsnow.com/" target="_blank" rel="noopener">THE SAVVY INSPECTOR</a>';
+    return '&copy; Copyright ' . date('Y') . ' ' . get_bloginfo('name') . '<br/>All Rights Reserved | Created By: <a href="https://getmemorehomeinspectionsnow.com/lead-generation-websites/" target="_blank" rel="noopener">THE SAVVY INSPECTOR</a>';
 
 }
 add_shortcode( 'copyright', 'get_copyright' );
@@ -38,7 +38,7 @@ add_shortcode( 'copyright-only', 'get_copyright_only' );
 
 function get_created_by() {
 
-    return 'Created By: <a href="https://getmemorehomeinspectionsnow.com/" target="_blank" rel="noopener">THE SAVVY INSPECTOR</a>';
+    return 'Created By: <a href="https://getmemorehomeinspectionsnow.com/lead-generation-websites/" target="_blank" rel="noopener">THE SAVVY INSPECTOR</a>';
 
 }
 add_shortcode( 'created-by', 'get_created_by' );
